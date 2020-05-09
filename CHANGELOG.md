43094749ef80e57d1d7afcff7ef2a3eebc2c026a
Athos Ribeiro <athoscr@fedoraproject.org>
2020-05-09 02:18:03 +0200

    Add form validations
    Signed-off-by: Athos Ribeiro <athoscr@fedoraproject.org>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,7 +8,7 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 - Respect browser and system proxy settings when noturno is off
 
 ### Changed
-- Minor changes to UI
+- New UI
 
 ### Removed
 - Removed reset button
--- menu/menu.js
+++ menu/menu.js
@@ -4,6 +4,9 @@ var host = document.getElementById('host');
 var port = document.getElementById('port');
 var type = document.getElementById('type');
 var form = document.getElementById('settings-form');
+var typeErr = document.getElementById("error-type")
+var hostErr = document.getElementById("error-host")
+var portErr = document.getElementById("error-port")
 
 function init(page) {
   page.getProxyConfig();
@@ -33,8 +36,49 @@ var get_background = browser.runtime.getBackgroundPage();
 /* initialize values */
 get_background.then(init, onError);
 
+host.oninput = function() {
+  hostErr.setAttribute("class", "text-error")
+  host.setCustomValidity("");
+}
+
+port.oninput = function() {
+  portErr.setAttribute("class", "text-error")
+  port.setCustomValidity("");
+}
+
+type.onchange = function() {
+  typeErr.setAttribute("class", "text-error")
+  type.setCustomValidity("");
+}
+
 form.onsubmit = function() {
+  var valid = true
+  if(!port.value) {
+    var errMsg = "Port should not be blank"
+    port.setCustomValidity(errMsg);
+    portErr.innerHTML = errMsg
+    portErr.setAttribute("class", "text-error display-block")
+    valid = false
+  }
+  if(!host.value) {
+    var errMsg = "Host should not be blank"
+    host.setCustomValidity(errMsg);
+    hostErr.innerHTML = errMsg
+    hostErr.setAttribute("class", "text-error display-block")
+    valid = false
+  }
+  if(!type.value) {
+    var errMsg = "Select a protocol"
+    type.setCustomValidity(errMsg);
+    typeErr.innerHTML = errMsg
+    typeErr.setAttribute("class", "text-error display-block")
+    valid = false
+  }
+  if(!valid) {
+    return false
+  }
   get_background.then(save, onError);
+  return true
 }
 
 toggle_switch.onclick = function() {
